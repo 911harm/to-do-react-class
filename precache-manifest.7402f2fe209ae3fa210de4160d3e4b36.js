8b04b70dfef9cd3b799bf6520687df193e0380fa
911harm <911harm@gmail.com>
2020-10-07 14:18:48 -0500

    Updates

---
+++ precache-manifest.7402f2fe209ae3fa210de4160d3e4b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e174b14aac19cc5007451810bf37a78",
+    "url": "/to-do-react-class/index.html"
+  },
+  {
+    "revision": "66893b27614ebc09e635",
+    "url": "/to-do-react-class/static/css/2.c3d9beca.chunk.css"
+  },
+  {
+    "revision": "66893b27614ebc09e635",
+    "url": "/to-do-react-class/static/js/2.12d6bd94.chunk.js"
+  },
+  {
+    "revision": "1a17c67f4311437e33b522bcde67da07",
+    "url": "/to-do-react-class/static/js/2.12d6bd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b2efcb5e12639c9ae5",
+    "url": "/to-do-react-class/static/js/main.fa9fdfd9.chunk.js"
+  },
+  {
+    "revision": "0ee177f7c21643810ca4",
+    "url": "/to-do-react-class/static/js/runtime-main.1a4f04c1.js"
+  }
+]);
